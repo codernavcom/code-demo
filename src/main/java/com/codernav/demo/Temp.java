6a2d84728e58a978210688a8031ba9159897798e
codernav.com <w17625991216@163.com>
2023-01-30 23:37:08 +0800

    新增文件

---
+++ src/main/java/com/codernav/demo/Temp.java
@@ -0,0 +1,11 @@
+package com.codernav.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println("temp");
+        List<String> list = new ArrayList<>();
+    }
+}
