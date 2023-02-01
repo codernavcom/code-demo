7672d4d4851fbbe5145c4e87e88543af47d15780
codernav.com <w17625991216@163.com>
2023-02-01 22:40:07 +0800

    新增数组操作

--- src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_26.java
+++ src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_26.java
@@ -1,14 +1,35 @@
 package com.codernav.demo.leetcode.array.removeelement;
 
+import java.util.Arrays;
+
 /**
  * TODO 26. 删除有序数组中的重复项
  * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
  * 由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
  * 将最终结果插入 nums 的前 k 个位置后返回 k 。
  * 不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
  */
 public class Q_26 {
     public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int length = f(nums);
+        System.out.println(length);
 
+    }
+
+    private static int f(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+        for (int fast = 0; fast < nums.length - 1; fast++) {
+            System.out.println("slow:" + slow + ", fast:" + fast + ", nums:" + Arrays.toString(nums));
+            if (nums[fast] != nums[fast + 1]) {
+                slow++;
+                nums[slow] = nums[fast + 1];
+            }
+        }
+        return slow + 1;
     }
 }
--- src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_27.java
+++ src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_27.java
@@ -1,13 +1,33 @@
 package com.codernav.demo.leetcode.array.removeelement;
 
 /**
- * TODO 27. 移除元素
+ * 27. 移除元素
  * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
  * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
  * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * 原文地址：https://www.codernav.com/2816.html
+ * 更多算法详解：https://www.codernav.com
  */
 public class Q_27 {
     public static void main(String[] args) {
+        // 数组只能覆盖，不能删除元素
+        // 双指针.算法
+        int length = f(new int[]{3, 2, 2, 3}, 3);
+        System.out.println(length);
+    }
 
+    private static int f(int[] nums, int val) {
+        // 慢指针 可以看成新数组的元素集合
+        int slow = 0;
+        // 快指针 可以看成原数组的元素集合
+        for (int fast = 0; fast < nums.length; fast++) {
+            // 原数组元素不等于给定值时，就把这个元素赋值给新数组，等于给定值就跳过
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                // 慢指针加1
+                slow++;
+            }
+        }
+        return slow-1;
     }
 }
