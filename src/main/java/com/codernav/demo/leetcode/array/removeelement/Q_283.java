4ec9e1b54c843043f2d025d784235ebdc784c329
codernav.com <w17625991216@163.com>
2023-02-04 16:12:38 +0800

    https://www.codernav.com/2891.html

--- src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_283.java
+++ src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_283.java
@@ -1,12 +1,35 @@
 package com.codernav.demo.leetcode.array.removeelement;
 
+import java.util.Arrays;
+
 /**
- * TODO 283. 移动零
+ * 283. 移动零
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ * 原文地址：https://www.codernav.com/2816.html
+ * 更多算法详解：https://www.codernav.com
  */
 public class Q_283 {
     public static void main(String[] args) {
+        f(new int[]{0, 1, 0, 3, 12});
+    }
 
+    private static void f(int[] nums) {
+
+        // 第一次遍历，得到数组[1,3,12,3,12]，及slow值
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+
+        // 第二次遍历，将slow后面的元素置为0
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        System.out.println("slow:" + slow + ", nums:" + Arrays.toString(nums));
     }
 }
--- src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_844.java
+++ src/main/java/com/codernav/demo/leetcode/array/removeelement/Q_844.java
@@ -1,9 +1,42 @@
 package com.codernav.demo.leetcode.array.removeelement;
 
 /**
- * 844. 比较含退格的字符串
+ * 844. 比较含退格的字符串（比较暴力的算法）
  * 给定 s 和 t 两个字符串，当它们分别被输入到空白的文本编辑器后，如果两者相等，返回 true 。# 代表退格字符。
  * 注意：如果对空文本输入退格字符，文本继续为空。
+ * 原文地址：https://www.codernav.com/2891.html
+ * 更多算法详解：https://www.codernav.com
  */
 public class Q_844 {
+    public static void main(String[] args) {
+        f("#ab#c#d", "ad#c");
+    }
+
+    private static boolean f(String s, String t) {
+
+        // 如果字符串中包含#，对字符串进行处理
+        while (s.contains("#")) {
+            int pos = s.indexOf("#"); // #出现的位置，该方法只返回#出现的第一个位置
+            StringBuilder sb = new StringBuilder(s);
+            if (sb.indexOf("#") == 0) { // #在第一位单独处理，防止异常
+                sb.deleteCharAt(0);
+            } else {
+                sb.delete(pos - 1, pos + 1); // #不在第一位，移除#和它的前一位置
+            }
+            s = sb.toString(); // 重新给字符串赋值
+        }
+
+        while (t.contains("#")) {
+            int pos = t.indexOf("#");
+            StringBuilder sb = new StringBuilder(t);
+            if (sb.indexOf("#") == 0) {
+                sb.deleteCharAt(0);
+            } else {
+                sb.delete(pos - 1, pos + 1);
+            }
+            t = sb.toString();
+        }
+
+        return s.equals(t);
+    }
 }
