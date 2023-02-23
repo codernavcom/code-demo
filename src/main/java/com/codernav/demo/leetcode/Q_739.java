a9a54a3c87e0b5891681b5cfdbf94c63b9426633
codernav.com <w17625991216@163.com>
2023-02-23 22:44:48 +0800

    每日温度 - 单调栈

--- src/main/java/com/codernav/demo/leetcode/Q_739.java
+++ src/main/java/com/codernav/demo/leetcode/Q_739.java
@@ -31,8 +31,8 @@ public class Q_739 {
     // 单调栈 方法一
     public static int[] dailyTemperatures(int[] temperatures) {
 
-        int lens=temperatures.length;
-        int []res=new int[lens];
+        int lens = temperatures.length;
+        int[] res = new int[lens];
 
         /**
          如果当前遍历的元素 大于栈顶元素，表示 栈顶元素的 右边的最大的元素就是 当前遍历的元素，
@@ -41,38 +41,38 @@ public class Q_739 {
          否则的话，可以直接入栈。
          注意，单调栈里 加入的元素是 下标。
          */
-        Deque<Integer> stack=new LinkedList<>();
+        Deque<Integer> stack = new LinkedList<>();
         stack.push(0);
-        for(int i=1;i<lens;i++){
+        for (int i = 1; i < lens; i++) {
 
-            if(temperatures[i]<=temperatures[stack.peek()]){
+            if (temperatures[i] <= temperatures[stack.peek()]) {
                 stack.push(i);
-            }else{
-                while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
-                    res[stack.peek()]=i-stack.peek();
+            } else {
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                    res[stack.peek()] = i - stack.peek();
                     stack.pop();
                 }
                 stack.push(i);
             }
         }
 
-        return  res;
+        return res;
     }
 
     // 单调栈 方法二
     public static int[] dailyTemperatures2(int[] temperatures) {
-        int lens=temperatures.length;
-        int []res=new int[lens];
-        Deque<Integer> stack=new LinkedList<>();
-        for(int i=0;i<lens;i++){
+        int lens = temperatures.length;
+        int[] res = new int[lens];
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < lens; i++) {
 
-            while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
-                res[stack.peek()]=i-stack.peek();
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                res[stack.peek()] = i - stack.peek();
                 stack.pop();
             }
             stack.push(i);
         }
 
-        return  res;
+        return res;
     }
 }
