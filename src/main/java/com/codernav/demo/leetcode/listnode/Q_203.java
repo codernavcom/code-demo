d1f3ec1a21f03114feaafd59bbb762f4ec89bdf4
codernav.com <w17625991216@163.com>
2023-02-26 22:33:42 +0800

    链表反转

--- src/main/java/com/codernav/demo/leetcode/listnode/Q_203.java
+++ src/main/java/com/codernav/demo/leetcode/listnode/Q_203.java
@@ -1,5 +1,7 @@
 package com.codernav.demo.leetcode.listnode;
 
+import com.codernav.demo.common.ListNode;
+
 /**
  * @title 203. 移除链表元素
  * @Description 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
@@ -9,6 +11,28 @@ package com.codernav.demo.leetcode.listnode;
  */
 public class Q_203 {
     public static void main(String[] args) {
+        // 1,2,6,3,4,5,6
+        ListNode node7 = new ListNode(6, null);
+        ListNode node6 = new ListNode(5, node7);
+        ListNode node5 = new ListNode(4, node6);
+        ListNode node4 = new ListNode(3, node5);
+        ListNode node3 = new ListNode(6, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        ListNode newHead = f(node1, 6);
 
+    }
+
+    private static ListNode f(ListNode head, int val) {
+        ListNode pre = null;
+        while (head.next != null) {
+            if (head.val == val) {
+                pre.next = head.next;
+            } else {
+                pre = head;
+            }
+            head = head.next;
+        }
+        return head;
     }
 }
--- src/main/java/com/codernav/demo/leetcode/listnode/reverseList/Q_206.java
+++ src/main/java/com/codernav/demo/leetcode/listnode/reverseList/Q_206.java
@@ -1,54 +1,41 @@
-package com.codernav.demo.hwod;
+package com.codernav.demo.leetcode.listnode.reverseList;
+
+import com.codernav.demo.common.ListNode;
 
 /**
- * 使用迭代方式解决“链表反转”问题
- * 题目：给定一个单链表的头结点pHead(该头节点是有值的，比如在下图，它的val是1)，长度为n，反转该链表后，返回新链表的表头。
- * 原文地址：https://www.codernav.com/2775.html
+ * @title 206. 反转链表
+ * @Description 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * @Author 开发者导航
+ * @website https://www.codernav.com
+ * @date 2023/2/26
  */
-public class OdTest04 {
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        public ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-    }
-
+public class Q_206 {
     public static void main(String[] args) {
-        // node1 -> node2 -> node3 -> node4 -> node5
+        // 输入：head = [1,2,3,4,5] 输出：[5,4,3,2,1]
         ListNode node5 = new ListNode(5, null);
         ListNode node4 = new ListNode(4, node5);
         ListNode node3 = new ListNode(3, node4);
         ListNode node2 = new ListNode(2, node3);
         ListNode node1 = new ListNode(1, node2);
-        ListNode prev = f(node1);
-        System.out.println(prev);
+        // 迭代
+        System.out.println(f(node1));
+        // 递归
+        System.out.println(f1(node1));
     }
 
-    /**
-     * 递归方式
-     * @param head
-     * @return
-     */
     public static ListNode f1(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        // 递归的方式找到最后一个元素
-        ListNode newHead = f1(head.next);
+        // 递归的目的是找到最后一个元素，从后往前执行下面的算法，如果从前往后执行，会导致执行一次算法后，链接断开
+        ListNode newHead = f(head.next);
+        // 将后一个节点的next节点指向head
         head.next.next = head;
+        // 将后一个节点指向null
         head.next = null;
         return newHead;
     }
 
-    /**
-     * 迭代方式
-     * @param head
-     * @return
-     */
     public static ListNode f(ListNode head) {
         // 后一个节点
         ListNode next;
@@ -67,9 +54,6 @@ public class OdTest04 {
             // 把当前节点指向后一个节点
             curr = next;
         }
-
         return prev;
     }
 }
-
-
